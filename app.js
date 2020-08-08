863f7b82d25270d35b5810c042b15ccac86a5570
Titi <christianpham.vidpb@gmail.com>
2020-08-08 19:01:04 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+/////////////////////
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+///const arraydulieu = ["an uong", "ngu nghi", "di choi"];
+const conclusion = "Hoc xong roi!";
+//////////////KET NOI VOI DATABASE
+const url = "mongodb://localhost:27017/newDatabaseName";
+mongoose.connect(url, {
+  useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+
+});
+////-> databa nay dung de lam gi? Kiem tra ket noi toi database
+const databa = mongoose.connection;
+databa.on("open", () => console.log("connected to mongodb"));
+
+//////////////////TAO SCHEMA, COLLECTION VA OPERATION
+const newSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+  },
+  done: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Activity = mongoose.model("Activity", newSchema);
+
+////////////////////
+app.get("/", function(req,res) {
+
+  ///res.render("index", {dulieu: arraydulieu, loiket: conclusion});///day la phan gianh cho ejs
+  const postDulieu = Activity.find();  ///du lieu tra ve la mot array
+  res.render("index", {dulieu: postDulieu});
+
+});
+
+app.post("/", async function(req,res) {
+  const newActivity = new Activity({name: req.body.nhapdulieu});
+  await newActivity.save();
+  res.redirect("/");
+});
+
+
+
+
+
+///app.post("/", (req,res) => {
+  ///const newData = req.body.nhapdulieu;
+  ///console.log(newData);
+  ///console.log(req.body.ten)
+  ///arraydulieu.push(newData);
+  ///res.redirect("/");
+///}); --Phan nay gianh cho ejs
+
+
+app.listen(3000, function() {
+  console.log("app is listening");
+});
